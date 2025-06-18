c36c5cd4c704eada12a9373f74bc75284cb505ba
pacifi <pacifi.bnr@gmail.com>
2025-06-17 20:16:59 -0500

    customer

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import datetime
+import uuid
 import zoneinfo
 
 from fastapi import FastAPI
+
+from models import Customer, Transaction, Invoice
 
 app = FastAPI()
 
@@ -25,7 +28,19 @@ async def time(iso_code: str):
     print(iso_code)
     code = country_timezones.get(iso_code.upper())
     tz = zoneinfo.ZoneInfo(code)
-
-    
     return {"time": datetime.datetime.now(tz)}
 
+
+@app.post("/customers")
+async def create_customer(customer_data: Customer):
+    return customer_data
+
+
+@app.post("/transactions")
+async def create_transaction(transaction_data: Transaction):
+    return transaction_data
+
+
+@app.post("/invoices")
+async def create_invoice(invoice_data: Invoice):
+    return invoice_data
